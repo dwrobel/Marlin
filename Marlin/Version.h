266906484f6b92f91017720cd8c0937418b6fe1f
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2017-08-31 17:32:37 +0200

    1.1.5 rebase

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,14 +41,14 @@
    * Verbose version identifier which should contain a reference to the location
    * from where the binary was downloaded or the source code was compiled.
    */
-  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (Github)"
+  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (Github branch:tip_1.1.5)"
 
   /**
    * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2017-08-28 12:00"
+  #define STRING_DISTRIBUTION_DATE "2017-08-31 17:31"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
